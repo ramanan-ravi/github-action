2bc8e0953fa4229b35d259aff95ac48c57bebb21
Ramanan Ravikumar <ramanan@deepfence.io>
2022-07-04 16:38:34 +0530

    Update

---
+++ yara-update/constants.py
@@ -0,0 +1,6 @@
+YARA_RULES_GITHUB_REPOS = [
+    "https://github.com/Neo23x0/signature-base/tree/master/yara",
+    "https://github.com/spyre-project/spyre",
+    "https://github.com/mandiant/red_team_tool_countermeasures/blob/master/all-yara.yar",
+    "https://github.com/volexity/threat-intel",
+    "https://github.com/Yara-Rules/rules"]
--- yara-update/yara-update.py
+++ yara-update/yara-update.py
@@ -1,2 +1,16 @@
+from constants import *
+from utils import *
+import git
+
+clone_folder = "/tmp/yara-rules"
+
+
+def parse_yara_rules(repo):
+    mkdir_all(clone_folder)
+    git.Git(clone_folder).clone(repo)
+
+
 if __name__ == '__main__':
-    print("hello")
+    for repo in YARA_RULES_GITHUB_REPOS:
+        parse_yara_rules(repo)
+    print(os.listdir(clone_folder))
